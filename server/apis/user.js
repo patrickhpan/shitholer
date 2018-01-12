d94c802d8e743224934c90d7331cb8cf88d8095f
Patrick Pan <patrickpan@college.harvard.edu>
2018-01-12 11:03:58 -0500

    Implemented fb auth for getting profile picture

--- server/apis/user.js
+++
@@ -1,9 +0,0 @@
-const uuid = require('uuid')
-
-const login = () => Promise.resolve({
-    id: uuid()
-})
-
-module.exports = {
-    login
-}
--- server/entry.js
+++ server/entry.js
@@ -1,8 +1,21 @@
 const express = require('express');
-
-const user = require('./routes/user');
+const expressSession = require('express-session');
+const passport = require('./auth/init');
 
 let app = express.Router();
-app.use('/user', user);
+app.use(expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize())
+app.use(passport.session())
+
+const auth = require('./routes/auth');
+const fb = require('./routes/fb');
+
+let authRoute = auth(passport);
+app.use('/auth', authRoute)
+app.use('/fb', fb);
 
 module.exports = app;
